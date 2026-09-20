26d88a08fddf9df6a2f97fdcbd7bdf6bb2514634
heyimlilyx3 <msalexawill@gmail.com>
2026-09-20 12:49:07 -0700

    Commit

--- lab-11/main.cpp
+++ lab-11/main.cpp
@@ -6,11 +6,47 @@ struct album{
     string title;
     string artist;
     int numTracks;
-    string tracklist[numTracks];
+    string* tracklist;
     int year;
 };
 
+album* inputAlbums(){       // function to input albums and return pointer to array of albums
+
+    int numAlbums;          // input number of albums from user
+    cout << "Enter the number of albums: ";
+    cin >> numAlbums;
+
+    album* albums = new album[numAlbums];   // create array of albums using pointer
+
+    for (int i = 0; i < numAlbums; i++) {   // loop to input details for each album
+        cout << "Enter details for album " << (i + 1) << ":" << endl;
+        cout << "Title: ";
+        cin.ignore(); // fix cin problems
+        getline(cin, albums[i].title);
+        cout << "Artist: ";
+        getline(cin, albums[i].artist);
+        cout << "Number of tracks: ";
+        cin >> albums[i].numTracks;
+
+        albums[i].tracklist = new string[albums[i].numTracks];
+        cin.ignore(); // fix cin problems
+        for (int j = 0; j < albums[i].numTracks; j++) {
+            cout << "Track " << (j + 1) << ": ";
+            getline(cin, albums[i].tracklist[j]);
+        }
+
+        cout << "Year: ";
+        cin >> albums[i].year;
+    }
+
+    return albums;
+
+
+}
+
 int main() {
-    cout << "Hello, World!" << endl;
+
+    album* myAlbums = inputAlbums();
+
     return 0;
 }
