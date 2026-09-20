eee085bbd91c3474e9afe47c4f82673fc30d3dd1
heyimlilyx3 <msalexawill@gmail.com>
2026-09-20 12:42:25 -0700

    Commit

--- lab-11/main.cpp
+++ lab-11/main.cpp
@@ -2,6 +2,14 @@
 #include <string>
 using namespace std;
 
+struct album{
+    string title;
+    string artist;
+    int numTracks;
+    string tracklist[numTracks];
+    int year;
+};
+
 int main() {
     cout << "Hello, World!" << endl;
     return 0;
